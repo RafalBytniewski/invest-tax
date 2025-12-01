30c9a95ee2fb0d63375527b2b239035cdb95c919
Rafal <rafal.bytniewski@o2.pl>
2025-12-01 08:36:47 +0100

    Modified my-wallets page, added wallet detail with transactions and pie chart for assets value

--- app/Livewire/MyWallets.php
+++ app/Livewire/MyWallets.php
@@ -7,12 +7,38 @@ use App\Models\Wallet;
 
 class MyWallets extends Component
 {
+/*     public $chartLabels = [];
+    public $chartData = [];
+ */
+
+    public $wallets;
+    public $visibleTransactions = [];
+
+    public function mount()
+    {
+        $this->wallets = Wallet::with('transactions.asset.exchange')->get();
+
+        /* $this->loadChartData(); */
+    }
+/* 
+    public function loadChartData()
+    {
+        // przykładowe dane do testu wykresu
+        $this->chartLabels = ['Bitcoin', 'Ethereum', 'AAPL', 'TSLA'];
+        $this->chartData = [4500, 2500, 3000, 2000]; // wartości w USD
+    } */
+
+    public function toggleTransactions($assetId)
+    {
+        if (isset($this->visibleTransactions[$assetId])) {
+            unset($this->visibleTransactions[$assetId]);
+        } else {
+            $this->visibleTransactions[$assetId] = true;
+        }
+    }
+
     public function render()
     {
-        $wallets = Wallet::with('transactions')->get();
-
-        return view('livewire.my-wallets',[
-            'wallets' => $wallets
-        ]);
+        return view('livewire.my-wallets');
     }
 }
